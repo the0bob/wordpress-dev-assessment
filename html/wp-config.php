52f57a7adb660dc88ae6350a4f29596af28aac44
the0bob <bob@bobpalmer.info>
2018-09-08 01:16:23 -0500

    override db site host

--- html/wp-config.php
+++ html/wp-config.php
@@ -37,6 +37,9 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define( 'WP_HOME', 'http://golocal.bobpalmer.info' );
+define( 'WP_SITEURL', 'http://golocal.bobpalmer.info' );
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
